4ae65e779ae9c732cf348edade536d8f0f966c3a
brickvanroekel <55390382+brickvanroekel@users.noreply.github.com>
2023-02-24 13:49:54 +0100

    mindAR click test

--- js/interact.js
+++ js/interact.js
@@ -1,35 +1,26 @@
-AFRAME.registerComponent('accepts-clicks', {
-    init: function() {
-      this.el.addEventListener('touchend', handleClickEvent);
-      this.el.addEventListener('click', handleClickEvent);
-    },
-    tick: function() {
-      hideSpeechBubbleIfNoMarker();
-    }
-  });
-
-  function handleClickEvent() {
-    for (var i = 0; i < builders.length; i++) {
-      var builder = builders[i];
-      var builderMarker = document.querySelector("#" + builder.name + "-marker");
-      if (builderMarker && builderMarker.object3D.visible) {
-        if (searchForBuilderTool(builder)) {
-          toggleSpeechBubble(builder.successDialogue);
-        } else {
-          toggleSpeechBubble(builder.dialogue);
-        }
-        break;
-      }
-    }
-  }
-
-  function hideSpeechBubbleIfNoMarker() {
-    var shouldHide = true;
-    for (var i = 0; i < builders.length; i++) {
-      var builderMarker = document.querySelector("#" + builders[i].name + "-marker");
-      if (builderMarker && builderMarker.object3D.visible) {
-        shouldHide = false;
-        break;
-      }
-    }
-  };
+document.addEventListener("DOMContentLoaded", function() {
+	const sceneEl = document.querySelector('a-scene');
+	const arSystem = sceneEl.systems["mindar-image-system"];
+	const exampleTarget = document.querySelector('#example-target');
+	const examplePlane = document.querySelector('#example-plane');
+	// arReady event triggered when ready
+	sceneEl.addEventListener("arReady", (event) => {
+	  console.log("MindAR is ready")
+	});
+	// arError event triggered when something went wrong. Mostly browser compatbility issue
+	sceneEl.addEventListener("arError", (event) => {
+	  console.log("MindAR failed to start")
+	});
+	// detect target found
+	exampleTarget.addEventListener("targetFound", event => {
+	  console.log("target found");
+	});
+	// detect target lost
+	exampleTarget.addEventListener("targetLost", event => {
+	  console.log("target lost");
+	});
+	// detect click event
+	examplePlane.addEventListener("click", event => {
+	  console.log("plane click");
+	});
+});
