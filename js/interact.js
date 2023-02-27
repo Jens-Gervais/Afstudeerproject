0496ceb676093bbc22bf8a70e85f99995ea66e27
brickvanroekel <55390382+brickvanroekel@users.noreply.github.com>
2023-02-27 15:13:30 +0100

    tryout with markers

--- js/interact.js
+++ js/interact.js
@@ -1,52 +1,101 @@
-var interval_timer;
-document.addEventListener("DOMContentLoaded", function () {
-	const sceneEl = document.querySelector('a-scene');
-	const arSystem = sceneEl.systems["mindar-image-system"];
-	const exampleTarget = document.querySelector('.example-target');
-	const exampleBuilder = document.querySelector('.example-builder');
-	const exampleTool = document.querySelector('.example-tool')
-	// arReady event triggered when ready
-	sceneEl.addEventListener("arReady", (event) => {
-		console.log("MindAR is ready")
+AFRAME.registerComponent('accepts-clicks', {
+	init: function() {
+	  this.el.addEventListener('touchend', handleClickEvent);
+	  this.el.addEventListener('click', handleClickEvent);
+	},
+	tick: function() {
+	  hideSpeechBubbleIfNoMarker();
+	}
+  });
+  
+  function hideSpeechBubbleIfNoMarker() {
+	var speechBubble = document.querySelector(".speech-bubble");
+	if (speechBubble.style.display === 'none' || !speechBubble.style.display) return;
+  
+	var shouldHide = true;
+	builders.forEach(function(builder){
+	  var builderMarker = document.querySelector("#" + builder.name + "-marker");
+	  if (builderMarker && builderMarker.object3D.visible) shouldHide = false;
 	});
-	// arError event triggered when something went wrong. Mostly browser compatbility issue
-	sceneEl.addEventListener("arError", (event) => {
-		console.log("MindAR failed to start")
+  
+	tools.forEach(function(tool){
+	  var toolMarker = document.querySelector("#" + tool.name + "-marker");
+	  if (toolMarker && toolMarker.object3D.visible) shouldHide = false;
 	});
-	// detect target found
-	exampleTarget.addEventListener("targetFound", event => {
-		console.log("target found");
+  
+	if (shouldHide) speechBubble.style.display = 'none';
+  };
+  
+  function handleClickEvent() {
+	builders.forEach(function(builder) {
+	  var builderMarker = document.querySelector("#" + builder.name + "-marker");
+	  if (builderMarker && builderMarker.object3D.visible) {
+		if (searchForBuilderTool(builder)){
+		  toggleSpeechBubble(builder.successDialogue);
+		} else {
+		  toggleSpeechBubble(builder.dialogue);
+		}
+	  }
 	});
-	// detect target lost
-	exampleTarget.addEventListener("targetLost", event => {
-		console.log("target lost");
+  
+	tools.forEach(function(tool){
+	  var toolMarker = document.querySelector("#" + tool.name + "-marker");
+	  if (toolMarker && toolMarker.object3D.visible) {
+		toggleSpeechBubble(tool.dialogue);
+		if (!userState.hasBuilderTool(tool)) userState.addTool(tool);
+	  }
 	});
-	// detect click event
-	exampleBuilder.addEventListener("click", event => {
-		console.log("builder click");
-		builders.forEach(function (builder) {
-			var builderElem = document.querySelector("#" + builder.name + "-img");
-			if (builderElem && builderElem.object3D.visible) {
-				if (searchForBuilderTool(builder)) {
-					toggleSpeechBubble(builder.successDialogue);
-				} else {
-					toggleSpeechBubble(builder.dialogue);
-				}
-			}
-		})
-	})
+  }
 
-	exampleTool.addEventListener("click", event => {
-		console.log("tool click");
-		tools.forEach(function (tool) {
-			var toolElem = document.querySelector("#" + tool.name + "-img");
-			if (toolElem && toolElem.object3D.visible) {
-				toggleSpeechBubble(tool.dialogue);
-				if (!userState.hasBuilderTool(tool)) userState.addTool(tool);
-			}
-		});
-	});
-});
+// var interval_timer;
+// document.addEventListener("DOMContentLoaded", function () {
+// 	const sceneEl = document.querySelector('a-scene');
+// 	const arSystem = sceneEl.systems["mindar-image-system"];
+// 	const exampleTarget = document.querySelector('.example-target');
+// 	const exampleBuilder = document.querySelector('.example-builder');
+// 	const exampleTool = document.querySelector('.example-tool')
+// 	// arReady event triggered when ready
+// 	sceneEl.addEventListener("arReady", (event) => {
+// 		console.log("MindAR is ready")
+// 	});
+// 	// arError event triggered when something went wrong. Mostly browser compatbility issue
+// 	sceneEl.addEventListener("arError", (event) => {
+// 		console.log("MindAR failed to start")
+// 	});
+// 	// detect target found
+// 	exampleTarget.addEventListener("targetFound", event => {
+// 		console.log("target found");
+// 	});
+// 	// detect target lost
+// 	exampleTarget.addEventListener("targetLost", event => {
+// 		console.log("target lost");
+// 	});
+// 	// detect click event
+// 	exampleBuilder.addEventListener("click", event => {
+// 		console.log("builder click");
+// 		builders.forEach(function (builder) {
+// 			var builderElem = document.querySelector("#" + builder.name + "-img");
+// 			if (builderElem && builderElem.object3D.visible) {
+// 				if (searchForBuilderTool(builder)) {
+// 					toggleSpeechBubble(builder.successDialogue);
+// 				} else {
+// 					toggleSpeechBubble(builder.dialogue);
+// 				}
+// 			}
+// 		})
+// 	})
+
+// 	exampleTool.addEventListener("click", event => {
+// 		console.log("tool click");
+// 		tools.forEach(function (tool) {
+// 			var toolElem = document.querySelector("#" + tool.name + "-img");
+// 			if (toolElem && toolElem.object3D.visible) {
+// 				toggleSpeechBubble(tool.dialogue);
+// 				if (!userState.hasBuilderTool(tool)) userState.addTool(tool);
+// 			}
+// 		});
+// 	});
+// });
 
 
 function toggleSpeechBubble(dialogue) {
@@ -66,26 +115,21 @@ function searchForBuilderTool(builder) {
 	});
 };
 
-interval_timer = setInterval(function(){
-	console.log("hide speech method called")
-	var speechBubble = document.querySelector(".speech-bubble");
-	if (speechBubble.style.display === 'none' || !speechBubble.style.display) return;
+// interval_timer = setInterval(function(){
+// 	console.log("hide speech method called")
+// 	var speechBubble = document.querySelector(".speech-bubble");
+// 	if (speechBubble.style.display === 'none' || !speechBubble.style.display) return;
 
-	var shouldHide = true;
-	builders.forEach(function (builder) {
-		var builderMarker = document.querySelector("#" + builder.name + "-img");
-		if (builderMarker && builderMarker.object3D.visible) shouldHide = false;
-	});
+// 	var shouldHide = true;
+// 	builders.forEach(function (builder) {
+// 		var builderMarker = document.querySelector("#" + builder.name + "-img");
+// 		if (builderMarker && builderMarker.object3D.visible) shouldHide = false;
+// 	});
 
-	tools.forEach(function (tool) {
-		var toolMarker = document.querySelector("#" + tool.name + "-img");
-		if (toolMarker && toolMarker.object3D.visible) shouldHide = false;
-	});
+// 	tools.forEach(function (tool) {
+// 		var toolMarker = document.querySelector("#" + tool.name + "-img");
+// 		if (toolMarker && toolMarker.object3D.visible) shouldHide = false;
+// 	});
 
-	if (shouldHide) speechBubble.style.display = 'none';
-}, 5000);
-
-// function hideSpeechBubbleIfNoMarker() {
-	
-
-// };
+// 	if (shouldHide) speechBubble.style.display = 'none';
+// }, 5000);
